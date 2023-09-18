3940e0af41a3e10a65e0abfa87d785a21d60f9e5
Joel <ajiolajoel@gmail.com>
2023-09-18 20:01:11 +0100

    update to youtube-clone

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -16,7 +16,20 @@ const VideoCard = ({video: {id:
         sx={{width: 358, height: 180}}
         />
         <CardContent sx={{backgroundColor:'#1e1e1e', 
-    height:'106px'}}/>
+    height:'106px'}}>
+        <Link to={videoId ? `/video/${videoId}` : demoVideoUrl}>
+            <Typography variant='subtitle1' fontWeight="bold" color="#FFF">
+                {snippet?.title.slice(0, 60) || 
+                demoVideoTitle.slice(0, 60)}
+            </Typography>
+        </Link>
+        <Link to={snippet?.channelId ? `/channel/${snippet?.channelId}` : demoChannelUrl}>
+            <Typography variant='subtitle2' fontWeight="bold" color="gray">
+                {snippet?.channelTitle || demoChannelTitle}
+                <CheckCircle sx={{ fontSize: 12, color:'gray', ml:'5px'}}/>
+            </Typography>
+        </Link>
+        </CardContent>
         </Link>
     </Card>
   )
